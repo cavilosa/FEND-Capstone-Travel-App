941c2765d531a577104e158059771e517b49e436
cavilosa <cavilosa33@gmail.com>
2021-01-08 21:38:35 -0700

    Data fom weatherbit received

--- src/server/server.js
+++ src/server/server.js
@@ -82,12 +82,18 @@ async function getGeoInfo() {
     }
 }
 
-async function weatherbit() {
-    console.log(geoData, inputData)
-    const url = `https://api.weatherbit.io/v2.0/history/daily&lat=${geoData.latitude}&lon=${geoData.longitude}&start_date=${inputData.departure}&end_date=${inputData.comeback}&key=${weather_key}`
-    console.log(url)
-
+async function weatherbit(req, res) {
+    console.log(weather_key)
+    const url = `https://api.weatherbit.io/v2.0/history/daily?&lat=${geoData.latitude}&lon=${geoData.longitude}&start_date=${inputData.departure}&end_date=${inputData.comeback}&key=${weather_key}`
+    const response = await fetch(url)
+        if (response.status !=200) {
+            console.log(response.status)
+        }
+    const data = await response.json();
+    console.log("data", data)
+    return data;
 }
+
 
 
 
