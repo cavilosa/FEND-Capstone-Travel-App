e4d2457b7c8bf5cb271c765c88ad9e50eeb190ff
cavilosa <cavilosa33@gmail.com>
2021-01-14 13:45:46 -0700

    Local storage moved to index.js file and works ok

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import { generate, getData, postData, getProjectData, upperCaseFirstChar, updateUI } from "./js/weather.js";
-import { getStorageData, checkStorage } from "./js/localStorage.js"
+//import { getStorageData, checkStorage } from "./js/localStorage.js"
 import "./styles/main.scss";
 import "./styles/header.scss";
 import "./styles/newtrip.scss";
@@ -12,7 +12,25 @@ picture.src = img
 document.getElementById("new-save").addEventListener("click", generate);
 
 window.addEventListener("load", checkStorage)
+async function checkStorage() {
+    if (!localStorage.getItem("projectData")) {
+        console.log("no local sotrage data")
+    }
+    //console.log(localStorage)
+    try {
+        getStorageData()
+        .then ( async (projectData) => {
+            updateUI(projectData)
+        })
+    }catch(e) {
+        console.log(e)
+    }
+}
 
+async function getStorageData(){
+    const projectData = JSON.parse(localStorage.getItem("projectData"))
+    return projectData
+}
 
 export {
     generate,
--- src/client/js/localStorage.js
+++ src/client/js/localStorage.js
@@ -1,8 +1,8 @@
-
 export async function checkStorage() {
     if (!localStorage.getItem("projectData")) {
         console.log("no local sotrage data")
     }
+    //console.log(localStorage)
     try {
         getStorageData()
         .then ( async (projectData) => {
@@ -17,50 +17,3 @@ export async function getStorageData(){
     const projectData = JSON.parse(localStorage.getItem("projectData"))
     return projectData
 }
-
-/*
-    try {
-        getStorageData()
-        .then ( async (projectData) => {
-            console.log(projectData)
-            Client.updateUI(projectData)
-        })
-    }catch(e) {
-        console.log(e)
-    }
-}
-
-export async function getStorageData(){
-    const projectData = JSON.parse(localStorage.getItem("projectData"))
-    return projectData
-}
-
-/*
-export async function updateUI (projectData) {
-
-    let destination = decodeURIComponent(projectData.inputData.destination)
-    upperCaseFirstChar(destination)
-
-    .then ((destination)=>{
-
-        document.querySelector(".destination").innerText = `My trip to: ${destination}`;
-
-        const pictureDiv = document.querySelector(".picture")
-        const url = projectData.picture
-        document.querySelector("img").src = url;
-
-    })
-}
-
-export async function upperCaseFirstChar (string) {
-    const words = string.split(" ");
-
-    for ( let i = 0; i < words.length; i++) {
-        let word = words[i];
-        word = word[0].toUpperCase() + word.slice(1)
-        console.log(word)
-        words[i] = word
-    }
-    console.log(words)
-    return words.join(" ")
-}*/
