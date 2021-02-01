6c3de03f1899a2d37c62c14b2720f4cdbfab9c85
cavilosa <cavilosa33@gmail.com>
2021-02-01 12:14:15 -0700

    Date formated in weatherforecast

--- src/client/js/weatherForecast.js
+++ src/client/js/weatherForecast.js
@@ -64,30 +64,48 @@ export async function updateWeatherUI(data) {
     const forecast = await Object.values(data);
     console.log(forecast)
 
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+
     const container = document.querySelector(".forecast")
     container.parentElement.scrollIntoView();
     if (container.children.length > 0){
         container.innerHTML = "";
         //console.log("empty containter data", data)
    } else {
-       container.innerHTML = "";
-       for (let y = 0; y <=3; y ++){ // creates rows
-           let row = document.createElement("div");
-           row.classList.add("row")
-           console.log("y", y)
-           //for (let z = 0; z <= 2 ; z++) { // creates cells
-           let x = y * 4;
-           let m = x + 3
-           for ( let z = x; z<=m; z++) {
-            console.log("1z", z)
-               let cell = document.createElement("div");
-               cell.classList.add("cell");
-               cell.innerHTML = `${forecast[z].date}<br>${forecast[z].description}
-                   <br>High: ${forecast[z].max_temp}&#176 C <br>
-                   Low: ${forecast[z].min_temp}&#176 C`
+        container.innerHTML = "";
+        for (let y = 0; y <=3; y ++){ // creates rows
+            let row = document.createElement("div");
+            row.classList.add("row")
+            console.log("y", y)
+            //for (let z = 0; z <= 2 ; z++) { // creates cells
+            let x = y * 4;
+            let m = x + 3
+            for ( let z = x; z<=m; z++) {
+                console.log("1z", z)
+                let cell = document.createElement("div");
+                cell.classList.add("cell");
+
+                console.log("forecast[z].date", forecast[z].date)
+                const l = forecast[z].date.toString()
+                const day = `${l[8]}${l[9]}`
+                console.log(day)
+
+                let dateMonth;
+                const month =  `${l[5]}${l[6]}`
+                if(month[0] === "0"){
+                    dateMonth = `${l[6]}`
+                } else {
+                    dateMonth = month
+                }
+                console.log(dateMonth, month)
+
+                cell.innerHTML = `${day} ${monthNames[dateMonth]}<br>${forecast[z].description}
+                   <br>High: ${forecast[z].max_temp}&#176C <br>
+                   Low: ${forecast[z].min_temp}&#176C`
                row.appendChild(cell);
            }
-       container.appendChild(row)
+        container.appendChild(row)
        }
 
     console.log("update ui end")
