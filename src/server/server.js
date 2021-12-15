d5508b913db504b20ca6ce5da9be6b0686962322
cavilosa <cavilosa33@gmail.com>
2021-12-15 13:08:57 -0700

    Added logging to serve.js with port var.

--- src/server/server.js
+++ src/server/server.js
@@ -30,7 +30,7 @@ app.get("/", function (req, res) {
 })
 
 const server = app.listen(port, () => {
-    console.log("Server is running on port 8080");
+    console.log(`Server is running on port ${PORT}`);
 })
 
 // Main project object that stores all the information
