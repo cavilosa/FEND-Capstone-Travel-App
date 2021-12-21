e376a357b2b68fc1e164c86a1a322fdf16ab9f7d
cavilosa <cavilosa33@gmail.com>
2021-12-21 11:46:37 -0700

    Corrected typos.

--- src/client/js/generateInfo.js
+++ src/client/js/generateInfo.js
@@ -63,7 +63,7 @@ export async function getData() {
 // Sending input to server side for further api calls
 export async function postData (data) {
     if (data !== undefined) {
-        const request = await fetch("/data", {
+        const request = await fetch("http://localhost:8081/data", {
             method: "POST",
             mode: "cors",
             headers: {
@@ -85,7 +85,7 @@ export async function postData (data) {
 
 // Getting main project object with all the information received from the APIs: geo, picture, input
 export async function getProjectData(){
-    const request = await fetch("/all");
+    const request = await fetch("http://localhost:8081/all");
     try{
         const data = await request.json();
         // If API received no geo info, will trigger the alert on the client side to change the destination
